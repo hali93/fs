5919b7a080d4c59f82cb14cdcd99e7843fe91c69
Halina <gella93@gmail.com>
2017-10-17 10:48:54 +0200

    update

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var fs = require('fs');
 
 fs.readdir('./', 'utf-8', function(err, files) {
 	console.log(files);
-	var dir = files;
-	fs.writeFile('text.txt', dir, function(err) {
+	var list = '';
+	for(var i = 0; i < files.length; i++) {
+		list += files[i] + '\r\n';
+	}
+
+	fs.writeFile('text.txt', list, function(err) {
 	if(err) throw err;
 	console.log('Saved!');
 	});
